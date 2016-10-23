6fdb36dd7ae62b61c253b7b61ed0e224403945f4
ssriram83 <ssriram83@gmail.com>
2016-10-23 18:29:12 +0530

    Changes for 23 Oct

---
+++ plugins/job-implementation.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Joi = require('joi');
+
+let internals = {};
+
+// Implement /jobs POST -- create job.. 
+
+// Params: Name, enabled, schedule -- later js style. method, tasks
+//https://github.com/firstandthird/hapi-job-queue
+
+
+module.exports = notification_delivery_status  
+
+
+
